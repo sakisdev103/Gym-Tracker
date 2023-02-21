d195816c13614f64985db46b0391712617aa3224
Your Name <you@example.com>
2023-02-21 20:16:56 +0200

    first commit

--- actions/dbconnect.php
+++ actions/dbconnect.php
@@ -1,3 +1,3 @@
-<?php 
+<?php
 
-$conn= new mysqli('localhost','root','','gym_tracker')or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli('sql302.epizy.com', 'epiz_33640594', 'Thanos1320372', 'epiz_33640594_gym_tracker') or die("Could not connect to mysql" . mysqli_error($con));
